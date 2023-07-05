e80a0a8b17bfae95f123e3aa634385871df63538
isnanaOne <1onlyonehana@gmail.com>
2023-07-05 16:18:41 +0900

    first commit

---
+++ src/main/java/com/susan/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.susan.controller;
+
+public class LoginController {
+    // login -> main
+    // 회원가입으로 이동(jsp에서 a태그로 처리)
+    // 로그인 하면 메인으로 이동
+    // id, password 찾기
+
+}
---
+++ src/main/java/com/susan/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.susan.controller;
+
+//(Page)에서 사용자가 입력을 받아서 처리하는 곳
+public class MainController {
+    // login으로 이동
+
+}
